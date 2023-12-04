67910e8dda588665b12bcc51cf9b9264409474e9
AhmedHesham1299 <127687214+AhmedHesham1299@users.noreply.github.com>
2023-12-04 16:21:42 +0200

    Project Initialized

---
+++ OpenglmeshLoader/OpenGLMeshLoader.cpp
@@ -0,0 +1,401 @@
+#include "TextureBuilder.h"
+#include "Model_3DS.h"
+#include "GLTexture.h"
+#include <glut.h>
+
+int WIDTH = 1280;
+int HEIGHT = 720;
+
+GLuint tex;
+char title[] = "3D Model Loader Sample";
+
+// 3D Projection Options
+GLdouble fovy = 45.0;
+GLdouble aspectRatio = (GLdouble)WIDTH / (GLdouble)HEIGHT;
+GLdouble zNear = 0.1;
+GLdouble zFar = 100;
+
+class Vector
+{
+public:
+	GLdouble x, y, z;
+	Vector() {}
+	Vector(GLdouble _x, GLdouble _y, GLdouble _z) : x(_x), y(_y), z(_z) {}
+	//================================================================================================//
+	// Operator Overloading; In C++ you can override the behavior of operators for you class objects. //
+	// Here we are overloading the += operator to add a given value to all vector coordinates.        //
+	//================================================================================================//
+	void operator +=(float value)
+	{
+		x += value;
+		y += value;
+		z += value;
+	}
+};
+
+Vector Eye(20, 5, 20);
+Vector At(-10, 0, -10);
+Vector Up(0, 1, 0);
+
+int cameraZoom = 0;
+
+// Model Variables
+Model_3DS model_Crystal;
+Model_3DS model_Asteriod;
+Model_3DS model_WarmHole;
+Model_3DS model_astronaut;
+
+
+// Textures
+GLTexture tex_ground;
+
+//=======================================================================
+// Lighting Configuration Function
+//=======================================================================
+void InitLightSource()
+{
+	// Enable Lighting for this OpenGL Program
+	glEnable(GL_LIGHTING);
+
+	// Enable Light Source number 0
+	// OpengL has 8 light sources
+	glEnable(GL_LIGHT0);
+
+	// Define Light source 0 ambient light
+	GLfloat ambient[] = { 0.1f, 0.1f, 0.1, 1.0f };
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+
+	// Define Light source 0 diffuse light
+	GLfloat diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+
+	// Define Light source 0 Specular light
+	GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+
+	// Finally, define light source 0 position in World Space
+	GLfloat light_position[] = { 0.0f, 10.0f, 0.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+}
+
+//=======================================================================
+// Material Configuration Function
+//======================================================================
+void InitMaterial()
+{
+	// Enable Material Tracking
+	glEnable(GL_COLOR_MATERIAL);
+
+	// Sich will be assigneet Material Properties whd by glColor
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+
+	// Set Material's Specular Color
+	// Will be applied to all objects
+	GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
+
+	// Set Material's Shine value (0->128)
+	GLfloat shininess[] = { 96.0f };
+	glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+}
+
+//=======================================================================
+// OpengGL Configuration Function
+//=======================================================================
+void myInit(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+
+	glMatrixMode(GL_PROJECTION);
+
+	glLoadIdentity();
+
+	gluPerspective(fovy, aspectRatio, zNear, zFar);
+	//*******************************************************************************************//
+	// fovy:			Angle between the bottom and top of the projectors, in degrees.			 //
+	// aspectRatio:		Ratio of width to height of the clipping plane.							 //
+	// zNear and zFar:	Specify the front and back clipping planes distances from camera.		 //
+	//*******************************************************************************************//
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glLoadIdentity();
+
+	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);
+	//*******************************************************************************************//
+	// EYE (ex, ey, ez): defines the location of the camera.									 //
+	// AT (ax, ay, az):	 denotes the direction where the camera is aiming at.					 //
+	// UP (ux, uy, uz):  denotes the upward orientation of the camera.							 //
+	//*******************************************************************************************//
+
+	InitLightSource();
+
+	InitMaterial();
+
+	glEnable(GL_DEPTH_TEST);
+
+	glEnable(GL_NORMALIZE);
+}
+
+//=======================================================================
+// Render Ground Function
+//=======================================================================
+void RenderMoon()
+{
+	glDisable(GL_LIGHTING);	// Disable lighting 
+
+
+	glEnable(GL_TEXTURE_2D);	// Enable 2D texturing
+
+	glBindTexture(GL_TEXTURE_2D, tex_ground.texture[0]);	// Bind the ground texture
+
+	glPushMatrix();
+	glBegin(GL_QUADS);
+	glNormal3f(0, 1, 0);	// Set quad normal direction.
+	glTexCoord2f(0, 0);		// Set tex coordinates ( Using (0,0) -> (5,5) with texture wrapping set to GL_REPEAT to simulate the ground repeated grass texture).
+	glVertex3f(-17, 0, -17);
+	glTexCoord2f(5, 0);
+	glVertex3f(17, 0, -17);
+	glTexCoord2f(5, 5);
+	glVertex3f(17, 0, 17);
+	glTexCoord2f(0, 5);
+	glVertex3f(-17, 0, 17);
+	glEnd();
+	glPopMatrix();
+
+	glEnable(GL_LIGHTING);	// Enable lighting again for other entites coming throung the pipeline.
+
+	glColor3f(1, 1, 1);	// Set material back to white instead of grey used for the ground texture.
+}
+
+//=======================================================================
+// Display Function
+//=======================================================================
+void myDisplay(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	RenderMoon();
+
+	// Draw Crystal
+	glPushMatrix();
+	glRotatef(90.f, 1, 0, 0);
+	glTranslatef(0, 0.0, 0); // Adjust the position as needed
+	model_Crystal.Draw();
+	glPopMatrix();
+
+	// Draw Asteroid
+	glPushMatrix();
+	glTranslatef(-10.0, 3.0, 0.0); // Adjust the position as needed
+	glScalef(0.01, 0.01, 0.01); // Adjust the scaling factor as needed
+
+	model_Asteriod.Draw();
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(-10.0, 3.0, -5.0); // Adjust the position as needed
+	glScalef(0.01, 0.01, 0.01); // Adjust the scaling factor as needed
+
+	model_Asteriod.Draw();
+	glPopMatrix();
+	/*
+	glPushMatrix();
+	glTranslatef(10.0, 3.0, 0.0); // Adjust the position as needed
+	glScalef(0.01, 0.01, 0.01); // Adjust the scaling factor as needed
+
+	model_Asteriod.Draw();
+	glPopMatrix();
+	*/
+	glPushMatrix();
+	glTranslatef(0.0, 3.0, 10.0); // Adjust the position as needed
+	glScalef(0.01, 0.01, 0.01); // Adjust the scaling factor as needed
+
+	model_Asteriod.Draw();
+	glPopMatrix();
+
+	// Draw Astronaut
+	glPushMatrix();
+	glRotatef(90.f, 0, 1, 0);
+	glTranslatef(-15, 3, 0); // Adjust the position as needed
+
+	// Scale down the astronaut model
+	glScalef(0.02, 0.02, 0.02); // Adjust the scaling factor as needed
+
+	model_astronaut.Draw();
+	glPopMatrix();
+
+	glPushMatrix();
+	glRotatef(90.f, 1, 0, 0);
+	glTranslatef(0.0, -20.0, 0); // Adjust the position as needed
+	model_WarmHole.Draw();
+	glPopMatrix();
+
+
+	glPushMatrix();
+
+	GLUquadricObj* qobj;
+	qobj = gluNewQuadric();
+	glTranslated(50, 0, 0);
+	glRotated(90, 1, 0, 1);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	gluQuadricTexture(qobj, true);
+	gluQuadricNormals(qobj, GL_SMOOTH);
+	gluSphere(qobj, 100, 100, 100);
+	gluDeleteQuadric(qobj);
+
+
+	glPopMatrix();
+
+	
+
+
+	glutSwapBuffers();
+}
+
+
+//=======================================================================
+// Keyboard Function
+//=======================================================================
+void myKeyboard(unsigned char button, int x, int y)
+{
+	switch (button)
+	{
+	case 'w':
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		break;
+	case 'r':
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		break;
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+//=======================================================================
+// Motion Function
+//=======================================================================
+void myMotion(int x, int y)
+{
+	y = HEIGHT - y;
+
+	if (cameraZoom - y > 0)
+	{
+		Eye.x += -0.1;
+		Eye.z += -0.1;
+	}
+	else
+	{
+		Eye.x += 0.1;
+		Eye.z += 0.1;
+	}
+
+	cameraZoom = y;
+
+	glLoadIdentity();	//Clear Model_View Matrix
+
+	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);	//Setup Camera with modified paramters
+
+	GLfloat light_position[] = { 0.0f, 10.0f, 0.0f, 1.0f };
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	glutPostRedisplay();	//Re-draw scene 
+}
+
+//=======================================================================
+// Mouse Function
+//=======================================================================
+void myMouse(int button, int state, int x, int y)
+{
+	y = HEIGHT - y;
+
+	if (state == GLUT_DOWN)
+	{
+		cameraZoom = y;
+	}
+}
+
+//=======================================================================
+// Reshape Function
+//=======================================================================
+void myReshape(int w, int h)
+{
+	if (h == 0) {
+		h = 1;
+	}
+
+	WIDTH = w;
+	HEIGHT = h;
+
+	// set the drawable region of the window
+	glViewport(0, 0, w, h);
+
+	// set up the projection matrix 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(fovy, (GLdouble)WIDTH / (GLdouble)HEIGHT, zNear, zFar);
+
+	// go back to modelview matrix so we can move the objects about
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(Eye.x, Eye.y, Eye.z, At.x, At.y, At.z, Up.x, Up.y, Up.z);
+}
+
+//=======================================================================
+// Assets Loading Function
+//=======================================================================
+void LoadAssets()
+{
+	// Loading Model files
+	model_Crystal.Load("Models/Crystal/0a0c75dab0844e7fa5b299d4af858bec.3ds");
+	model_WarmHole.Load("Models/WarmHole/33b7ab81fc1d4d6e8310bad1567b96da.3ds");
+	model_astronaut.Load("Models/Astronaut/Spaceman N100519.3ds");
+	model_Asteriod.Load("Models/Asteriod/ASTHANGR.3ds");
+
+	// Loading texture files
+	tex_ground.Load("Textures/download.bmp");
+	loadBMP(&tex, "Textures/download (1).bmp", true);
+}
+
+//=======================================================================
+// Main Function
+//=======================================================================
+void main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	glutInitWindowSize(1980, 1900);
+
+	glutInitWindowPosition(0, 0);
+
+	glutCreateWindow(title);
+
+	glutDisplayFunc(myDisplay);
+
+	glutKeyboardFunc(myKeyboard);
+
+	glutMotionFunc(myMotion);
+
+	glutMouseFunc(myMouse);
+
+	glutReshapeFunc(myReshape);
+
+	myInit();
+
+	LoadAssets();
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glShadeModel(GL_SMOOTH);
+
+	glutMainLoop();
+}
